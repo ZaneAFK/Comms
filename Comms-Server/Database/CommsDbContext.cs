19e8da603ad7fe552c32cae816eddc451cf0f33d
ZaneAFK <ztonitto@outlook.com>
2025-12-22 12:24:38 +1100

    Created domain user and security user models

---
+++ Comms-Server/Database/CommsDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Comms_Server.Database.Models.Users;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Comms_Server.Database
+{
+	public class CommsDbContext : IdentityDbContext<SecurityUser, IdentityRole<Guid>, Guid>
+	{
+		public CommsDbContext(DbContextOptions<CommsDbContext> options)
+			: base(options)
+		{
+		}
+
+		public DbSet<DomainUser> DomainUsers => Set<DomainUser>();
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			modelBuilder.Entity<DomainUser>()
+				.HasOne(u => u.SecurityUser)
+				.WithOne()
+				.HasForeignKey<DomainUser>(u => u.SecurityUserId)
+				.IsRequired();
+		}
+	}
+}
