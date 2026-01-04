e94c4a4a9e08bd2f7e70f5b32def15809b2cead5
ZaneAFK <ztonitto@outlook.com>
2026-01-04 13:48:43 +1100

    Added comment

--- Comms-Client/src/test/sampleTest.test.ts
+++ Comms-Client/src/test/sampleTest.test.ts
@@ -1,5 +1,7 @@
 import { expect, test } from 'vitest'
 
+// THIS IS A SAMPLE TEST SO CI HAS AT LEAST ONE TEST TO RUN
+// REMOVE THIS WHEN MORE TESTS ARE ADDED
 test('sample test', () => {
 	expect(1 + 1).toBe(2);
 })
