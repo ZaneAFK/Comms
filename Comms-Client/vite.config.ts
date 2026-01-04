2b1d5eb203d90dc41e78b44dc4926f0b58695da8
ZaneAFK <ztonitto@outlook.com>
2026-01-04 13:00:21 +1100

    Added vitest and updated CI to include frontend testing

--- Comms-Client/vite.config.ts
+++ Comms-Client/vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
@@ -10,11 +10,15 @@ export default defineConfig({
 	plugins: [
 		vue(),
 		vueDevTools(),
-		tailwindcss(),
+		tailwindcss()
 	],
+	test: {
+		environment: 'jsdom',
+		globals: true
+	},
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url))
-		},
+		}
 	},
 })
