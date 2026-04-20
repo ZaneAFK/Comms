553e5636d6565329e03cef70e9b844b048a0b56c
ZaneAFK <ztonitto@outlook.com>
2026-04-21 07:15:42 +1000

    Created basic temporary logout feature

--- Comms-Client/src/components/messages/ToolBar.vue
+++ Comms-Client/src/components/messages/ToolBar.vue
@@ -3,13 +3,25 @@
 		<div class="flex items-center gap-4">
 			<h1 class="text-white text-2xl font-bold">Comms</h1>
 		</div>
-		<div class="flex">
+		<div class="flex items-center gap-4">
 			<h2 v-if="props.username">{{ props.username }}</h2>
 			<h2 v-else>NULL USER</h2>
+			<!-- TODO: This logout feature should be in a settings menu. Move to there once available. -->
+			<button @click="handleLogout" class="logout-btn">Logout</button>
 		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
+	import { useAuthStore } from '@/stores/auth'
+	import { useRouter } from 'vue-router'
+
 	const props = defineProps(['username'])
+	const authStore = useAuthStore()
+	const router = useRouter()
+
+	function handleLogout() {
+		authStore.logout()
+		router.push('/login')
+	}
 </script>
